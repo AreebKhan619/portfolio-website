eed2cb19818b1c6e661eae65d8201b99bd059d23
Areeb Khan <areeb@emertech.io>
2022-05-15 19:50:29 +0530

    Removed unused code, added global css in js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,6 @@ import Navbar from "../components/shared/Navbar";
 import MainBody from "../components/shared/MainBody";
 import { OuterContainer } from "./styled";
 
-document.body.style.margin = 0;
-
 function App() {
   return (
     <OuterContainer>
